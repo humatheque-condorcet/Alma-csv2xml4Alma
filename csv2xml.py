ae23646bc24490fc965776dd58cd79e39bbe1a69
jszenb <jerome.chiavassa-szenberg@campus-condorcet.fr>
2019-04-03 16:36:20 +0200

    Test mapping CSV/XML ; captation d'un message d'erreur dans ecrire_bloc ; modification sur les appels d'add_ownered_entity

--- csv2xml.py
+++ csv2xml.py
@@ -5,7 +5,7 @@
 # DESCRIPTION : Ce traitement convertit un fichier CSV venu du référentiel 
 #               d'identité du Campus Condorcet en un fichier XML pouvant être
 #               importer dans Alma pour créer / mettre à jour un utilisateur.
-#               ATTENTION : le séparateur de champ dans le CSV doit être ';'
+#               ATTENTION : le séparateur de champ dans le CSV doit être ','
 #               La première du fichier CSV doit être contenir des balises à
 #               convertir. Le CSV doit être en UTF-8.
 # ENTREE      : nom du fichier CSV à convertir, répondant aux normes ci-dessus.
@@ -40,39 +40,60 @@ def add_ownered_entity(balise_mere):
     # Le bloc suivant est standard :
     balise_OE = ET.SubElement(balise_mere, "owneredEntity")
     my_node = ET.SubElement(balise_OE, "creationDate").text = time.strftime('%y%m%d',time.localtime())
+    my_node = ET.SubElement(balise_OE, "modificationDate").text = time.strftime('%y%m%d',time.localtime())
+    my_node = ET.SubElement(balise_OE, "modifiedBy").text = "Campus Condorcet GED csv2xml.py"
+    my_node = ET.SubElement(balise_OE, "createdBy").text = "Campus Condorcet"
     return (my_node)
 
-def ecrire_bloc(ma_colonne, ma_valeur):
+def ecrire_bloc(ma_colonne, ma_valeur, ma_ligne):
     """ ecrire_bloc : écriture d'un bloc dans la structure XML.
         ma_colonne permet de retrouver le tag XML a ajouter
         ma_valeur est la valeur que le tag XML porte """
-    print("Ajout du bloc suivant : ")
-    print("\tColonne CSV : ", ma_colonne)
-    print("\tma_valeur CSV : ", ma_valeur)
-    print("\tStructure XML correspondant à la colonne dans convert_tag :", ", ".join(convert_tag[ma_colonne]))
+    mon_msg = "\nAjout du bloc suivant : \n"
+    try:
+        mon_msg += "\tColonne CSV : " + ma_colonne + "\n"
+        mon_msg += "\tma_valeur CSV : " + ma_valeur + "\n"
+        mon_msg += "\tStructure XML correspondant à la colonne dans convert_tag :" + ", ".join(convert_tag[ma_colonne]) + "\n"
 
-    if len(convert_tag[ma_colonne]) == 3: # On travaille sur un tag dans un sous-bloc d'un bloc
-        # On crée d'abord le sous-bloc :
-        print("\ttag père : ", convert_tag[ma_colonne][2])
-        print("\ttag fils : ", convert_tag[ma_colonne][1])
-        print("\tvaleur : ", ma_valeur)
-        mon_bloc_pere = ET.SubElement(ref_bloc[convert_tag[ma_colonne][2]], convert_tag[ma_colonne][1])
-        # Puis le tag :
-        mon_bloc =  ET.SubElement(mon_bloc_pere, convert_tag[ma_colonne][0]).text = str(ma_valeur)   
-    elif len(convert_tag[ma_colonne]) == 2:
-        print("\ttag fils : ", convert_tag[ma_colonne][1])
-        print("\tvaleur : ", ma_valeur)
-        mon_bloc = ET.SubElement(ref_bloc[convert_tag[ma_colonne][1]], convert_tag[ma_colonne][0]).text = str(ma_valeur)  
-    return (mon_bloc)
+        if len(convert_tag[ma_colonne]) == 3: # On travaille sur un tag dans un sous-bloc d'un bloc
+            # On crée d'abord le sous-bloc :
+            mon_msg += "\ttag père : " + convert_tag[ma_colonne][2] + "\n"
+            mon_msg += "\ttag fils : " + convert_tag[ma_colonne][1] + "\n"
+            mon_msg += "\tvaleur : " + ma_valeur + "\n"
+            mon_bloc_pere = ET.SubElement(ref_bloc[convert_tag[ma_colonne][2]], convert_tag[ma_colonne][1])
+            # Puis le tag :
+            mon_bloc =  ET.SubElement(mon_bloc_pere, convert_tag[ma_colonne][0]).text = str(ma_valeur)   
+        elif len(convert_tag[ma_colonne]) == 2:
+            mon_msg += "\ttag fils : " + convert_tag[ma_colonne][1] + "\n"
+            mon_msg += "\tvaleur : " +  ma_valeur + "\n"
+            mon_bloc = ET.SubElement(ref_bloc[convert_tag[ma_colonne][1]], convert_tag[ma_colonne][0]).text = str(ma_valeur)  
+        
+        print (mon_msg)
+
+        return (mon_bloc)
+
+    except Exception as e:
+        mon_msg += "-- ERREUR --\n"
+        mon_msg += "Exception dans ecrire_bloc : " + str(e) + " \n"
+
+        if ma_valeur == "":
+            mon_msg += "Valeur nulle dans la colonne à la ligne n°" + str(ma_ligne) + " du fichier d'entrée \n"
+        else:
+            mon_msg += "Vérifiez les données à la ligne n°" + str(ma_ligne) + " du fichier d'entrée \n"
+
+        mon_msg += "------------"
+        print (mon_msg)
+        pass
 
 # **************************************************************************** #
-#                      Listes des tags par blocs dans XML                      #
+#                   Listes des colonnes CSV par blocs dans XML                 #
+# Rq. : on utilise cette liste pour construire le mapping CSV / XML ensuite    #
 # **************************************************************************** #
-list_tag_userDetails = ("Birthdate", "b", "c", "d", "e", "f", "g", "h", "i", "j")
-list_tag_owneredEntities = ("k", "l", "la", "m")
-list_tag_userIdentifier = ("n", "o", "p")
-list_tag_userAddress = ("q", "r", "s", "t")
-list_tag_userEmail = ("u", "v")
+list_tag_userDetails = ("", "givenName", "sn", "", "", "", "uid", "", "", "")
+list_tag_owneredEntities = ("", "", "", "")
+list_tag_userIdentifier = ("", "uid", "")
+list_tag_userAddress = ("postalAddress", "roomNumber", "", "")
+list_tag_userEmail = ("mail", "")
 list_tag_userCategory = ("w", "x")
 
 # **************************************************************************** #
@@ -81,36 +102,36 @@ list_tag_userCategory = ("w", "x")
 # **************************************************************************** #
 convert_tag={}
 # Bloc userDetails du XML
-convert_tag[list_tag_userDetails[0]] = ["birthDate", "userDetails"]
-convert_tag[list_tag_userDetails[1]] = ["firstName", "userDetails"]
-convert_tag[list_tag_userDetails[2]] = ["lastName", "userDetails"]
-convert_tag[list_tag_userDetails[3]] = ["status", "userDetails"]
-convert_tag[list_tag_userDetails[4]] = ["userType", "userDetails"]
-convert_tag[list_tag_userDetails[5]] = ["userGroup", "userDetails"]
-convert_tag[list_tag_userDetails[6]] = ["userName", "userDetails"]
-convert_tag[list_tag_userDetails[7]] = ["purgeDate", "userDetails"]
-convert_tag[list_tag_userDetails[8]] = ["defaultLanguage", "userDetails"]
-convert_tag[list_tag_userDetails[9]] = ["campusCode", "userDetails"]
-# Bloc owneredEntities du XML
+convert_tag[list_tag_userDetails[0]] = ["birthDate", "userDetails"]       # CSV : colonne non spécifiée
+convert_tag[list_tag_userDetails[1]] = ["firstName", "userDetails"]       # CSV : colonne givenName
+convert_tag[list_tag_userDetails[2]] = ["lastName", "userDetails"]        # CSV : colonne sn
+convert_tag[list_tag_userDetails[3]] = ["status", "userDetails"]          # CSV : colonne non spécifiée
+convert_tag[list_tag_userDetails[4]] = ["userType", "userDetails"]        # CSV : colonne non spécifiée
+convert_tag[list_tag_userDetails[5]] = ["userGroup", "userDetails"]       # CSV : colonne non spécifiée
+convert_tag[list_tag_userDetails[6]] = ["userName", "userDetails"]        # CSV : colonne uid OU METTRE firstName + lastName ??? 
+convert_tag[list_tag_userDetails[7]] = ["purgeDate", "userDetails"]       # CSV : colonne non spécifiée
+convert_tag[list_tag_userDetails[8]] = ["defaultLanguage", "userDetails"] # CSV : colonne non spécifiée
+convert_tag[list_tag_userDetails[9]] = ["campusCode", "userDetails"]      # CSV : colonne non spécifiée    
+# Bloc owneredEntities du XML : n'est pas déterminé par des valeurs du CSV d'entrée.
 convert_tag[list_tag_owneredEntities[0]] = ["creationDate", "owneredEntities"]
 convert_tag[list_tag_owneredEntities[1]] = ["modificationDate", "owneredEntities"]
 convert_tag[list_tag_owneredEntities[2]] = ["modifiedBy", "owneredEntities"]
 convert_tag[list_tag_owneredEntities[3]] = ["createdBy", "owneredEntities"]
 # Bloc userIdentifier du XML
-convert_tag[list_tag_userIdentifier[0]] = ["type", "userIdentifier"]
-convert_tag[list_tag_userIdentifier[1]] = ["value", "userIdentifier"]
-convert_tag[list_tag_userIdentifier[2]] = ["matchId", "userIdentifier"]
+convert_tag[list_tag_userIdentifier[0]] = ["type", "userIdentifier"]      # CSV : colonne non spécifiée
+convert_tag[list_tag_userIdentifier[1]] = ["value", "userIdentifier"]     # CSV : colonne uid
+convert_tag[list_tag_userIdentifier[2]] = ["matchId", "userIdentifier"]   # CSV : colonne non spécifiée
 # Bloc userAddress du XML
-convert_tag[list_tag_userAddress[0]] = ["line1", "userAddress"]
-convert_tag[list_tag_userAddress[1]] = ["line2", "userAddress"]
-convert_tag[list_tag_userAddress[2]] = ["note", "userAddress"]
-convert_tag[list_tag_userAddress[3]] = ["userAddressTypes", "types", "userAddress"]
+convert_tag[list_tag_userAddress[0]] = ["line1", "userAddress"]           # CSV : colonne postalAddress
+convert_tag[list_tag_userAddress[1]] = ["line2", "userAddress"]           # CSV : colonne roomNumber
+convert_tag[list_tag_userAddress[2]] = ["note", "userAddress"]            # CSV : colonne non spécifiée
+convert_tag[list_tag_userAddress[3]] = ["userAddressTypes", "types", "userAddress"] # CSV : colonne non spécifiée
 # Bloc userEmail du XML
-convert_tag[list_tag_userEmail[0]] = ["email", "userEmail"]
-convert_tag[list_tag_userEmail[1]] = ["userEmailTypes", "types", "userEmail"]
+convert_tag[list_tag_userEmail[0]] = ["email", "userEmail"]               # CSV : colonne non spécifiée
+convert_tag[list_tag_userEmail[1]] = ["userEmailTypes", "types", "userEmail"] # CSV : colonne non spécifiée
 # Bloc userCategory du XML
-convert_tag[list_tag_userCategory[0]] = ["statisticalCategory", "userCategory"]
-convert_tag[list_tag_userCategory[1]] = ["note", "userCategory"]
+convert_tag[list_tag_userCategory[0]] = ["statisticalCategory", "userCategory"] # CSV : colonne non spécifiée
+convert_tag[list_tag_userCategory[1]] = ["note", "userCategory"]                # CSV : colonne non spécifiée
 
 # Le dictionnaire suivant permettra de faire les passages d'objet aux fonctions.
 # Ce n'est pas très propre mais je ne vois pas comment faire autrement pour le
@@ -125,7 +146,7 @@ ref_bloc={}
 csv_file = csv_args (sys.argv)
 
 # Récupération des données d'entrée.
-csv_data = csv.reader(open(csv_file,'r', encoding="UTF-8"), delimiter=';')
+csv_data = csv.reader(open(csv_file,'r', encoding="UTF-8"), delimiter=',')
 
 # Fichier de sortie
 xml_file = 'importUserXML.xml'
@@ -195,27 +216,37 @@ for ligne in csv_data:
         
         # Remplissage de la structure XML
         for colonne in range(len(tags)):
-            bloc = ecrire_bloc(tags[colonne], str(ligne[colonne]))
+            bloc = ecrire_bloc(tags[colonne], str(ligne[colonne]), ligne_num)
             
             # Déterminer si on doit ajouter un sous-bloc owneredEntities : on
             # ne l'écrit qu'une seule fois dans les sous-blocs concernés
             if tags[colonne] in list_tag_userDetails and not b_userDetails:
-                bloc = add_ownered_entity(userDetails)
+                #bloc = add_ownered_entity(userDetails)
                 b_userDetails = True
                 nb_utilisateur_insere += 1
             elif tags[colonne] in list_tag_userIdentifier and not b_userIdentifier:
-                bloc = add_ownered_entity(userIdentifier)
+                #bloc = add_ownered_entity(userIdentifier)
                 b_userIdentifier = True
             elif tags[colonne] in list_tag_userAddress and not b_userAddress:
-                bloc = add_ownered_entity(userAddress)
+                #bloc = add_ownered_entity(userAddress)
                 b_userAddress = True
             elif tags[colonne] in list_tag_userEmail and not b_userEmail:
-                bloc = add_ownered_entity(userEmail)
+                #bloc = add_ownered_entity(userEmail)
                 b_userEmail = True
             elif tags[colonne] in list_tag_userCategory and not b_userCategory:
-                bloc = add_ownered_entity(userCategory)
+                #bloc = add_ownered_entity(userCategory)
                 b_userCategory = True
 
+        if b_userDetails:
+            bloc = add_ownered_entity(userDetails)
+        if b_userIdentifier:
+            bloc = add_ownered_entity(userIdentifier)
+        if b_userAddress:
+            bloc = add_ownered_entity(userAddress)
+        if b_userEmail:
+            bloc = add_ownered_entity(userEmail)
+        if b_userCategory:
+            bloc = add_ownered_entity(userCategory)
 
             """if tags[colonne] == "Birthdate":
                 node = ecrire_bloc(tags[colonne], str(ligne[colonne]))
